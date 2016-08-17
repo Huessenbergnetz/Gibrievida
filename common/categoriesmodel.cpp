6a29f807a4252cbab4b6b420d67ca23d93467884
Buschmann <kontakt@buschmann23.de>
2016-08-17 19:18:24 +0200

    Add copyright information to every source file.

--- common/categoriesmodel.cpp
+++ common/categoriesmodel.cpp
@@ -1,3 +1,21 @@
+/*
+    Gibrievida - An activity tracker
+    Copyright (C) 2016 Buschtrommel/Matthias Fehring http://gibrievida.buschmann23.de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "categoriesmodel.h"
 #include <QSqlQuery>
 #include "categoriescontroller.h"
